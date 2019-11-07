b80f6e39a4a6f92167a74bf42a43d94d74a12a20
kyrios9 <colesmith559@gmail.com>
2019-11-06 20:38:41 -0700

    added dot files

--- dots.sh
+++ dots.sh
@@ -11,7 +11,7 @@ cp ~/.yabairc ~/.dotfiles/
 cp ~/bin/wal.sh ~/.dotfiles/scripts/
 cp ~/bin/waltospice.jar ~/.dotfiles/scripts/
 
-git add *
+git add .
 
 timestamp=$(date +"%D %T")
 git commit -m "updated dots on $timestamp"
